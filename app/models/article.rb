43cd5366759231c8a8826117f522a16fded61ead
darenang44 <darenang44@gmail.com>
2016-12-22 13:20:23 -0800

    Add create action, view,  and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validate :title, precence: true, length: {minimum: 3, maximum: 50}
-  validate :description, precence: true, length: {minimum: 10, maximum: 500}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 500}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  resources :articles
 end
+
+# Prefix Verb   URI Pattern                  Controller#Action
+#         root GET    /                            pages#home
+#        about GET    /about(.:format)             pages#about
+#     articles GET    /articles(.:format)          articles#index
+#              POST   /articles(.:format)          articles#create
+#  new_article GET    /articles/new(.:format)      articles#new
+# edit_article GET    /articles/:id/edit(.:format) articles#edit
+#      article GET    /articles/:id(.:format)      articles#show
+#              PATCH  /articles/:id(.:format)      articles#update
+#              PUT    /articles/:id(.:format)      articles#update
+#              DELETE /articles/:id(.:format)      articles#destroy
