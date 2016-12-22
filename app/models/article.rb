58db37e712b9b5a622ef4c479a835ec6a05d07d1
darenang44 <darenang44@gmail.com>
2016-12-22 12:27:13 -0800

    Add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validate :title, precence: true
-  validate :description, precence: true
+  validate :title, precence: true, length: {minimum: 3, maximum: 50}
+  validate :description, precence: true, length: {minimum: 10, maximum: 500}
 end
